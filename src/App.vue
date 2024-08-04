4ef4b622f48cad9a100f824fd2b99580ea1d33cb
JackMorais <jack_h-22@hotmail.com>
2024-08-04 03:37:39 -0300

    atualizando e finalizando calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive ({
+  primeiroNumero: 0,
+  segundoNumero: 0,
+  filtro: 'somar',
+})
+function Somar(){
+  const { primeiroNumero, segundoNumero } = estado;
+  return parseInt(primeiroNumero) + parseInt(segundoNumero);
+}
+function Diminuir(){
+  const { primeiroNumero, segundoNumero } = estado;
+  return parseInt(primeiroNumero) - parseInt(segundoNumero);
+}
+function Multiplicar(){
+  const { primeiroNumero, segundoNumero } = estado;
+  return parseInt(primeiroNumero) * parseInt(segundoNumero);
+}
+function Dividir(){
+  const { primeiroNumero, segundoNumero } = estado;
+  return parseInt(primeiroNumero) / parseInt(segundoNumero);
+}
+
+const getOperacao = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+
+    case 'somar':
+      return Somar();
+    
+      case 'diminuir':
+      return Diminuir();
+    
+    case 'multiplicar':
+      return Multiplicar();
+
+    case 'dividir':
+      return Dividir();  
+
+    Default:
+      return Somar();
+  }
+}
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 bg-light rounded-3 ">
+      <h1>Calculadora Aritmetica</h1>
+      <p>Escolha um das 4 operações</p>
+    </header>
+    <form>
+      <div class="row">
+        <div class="col">
+          <input @keyup="evento => estado.primeiroNumero = evento.target.value" type="number" placeholder="primeiro numero" class="form-control mr-3a">
+          <input @keyup="evento => estado.segundoNumero = evento.target.value" type="number" placeholder="segundo numero" class="form-control">
+        </div>
+        <div class="col-md-4">
+          <select @change="evento => estado.filtro = evento.target.value" class="form-control">
+            <option value="somar">+</option>
+            <option value="diminuir">-</option>
+            <option value="multiplicar">x</option>
+            <option value="dividir">:</option>
+          </select>
+        </div>
+      </div>
+      Resultado: {{ getOperacao() }}
+    </form>
+    
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
